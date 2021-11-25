d00184440c45561a6c0df3fd08fee3e77c4a3aab
kouedelo <kouedelo37@gmail.com>
2021-11-25 14:06:01 -0500

    Creation of model package with various classes

--- src/model/Cash.java
+++ src/model/Cash.java
@@ -1,4 +1,49 @@
 package model;
 
 public class Cash {
+    private String paymentId;
+    private int numberOfBankNotes;
+    private String typeOfBankNote;
+
+    public Cash() {
+    }
+
+    public Cash(String paymentId, int numberOfBankNotes, String typeOfBankNote) {
+        this.paymentId = paymentId;
+        this.numberOfBankNotes = numberOfBankNotes;
+        this.typeOfBankNote = typeOfBankNote;
+    }
+
+    public String getPaymentId() {
+        return paymentId;
+    }
+
+    public void setPaymentId(String paymentId) {
+        this.paymentId = paymentId;
+    }
+
+    public int getNumberOfBankNotes() {
+        return numberOfBankNotes;
+    }
+
+    public void setNumberOfBankNotes(int numberOfBankNotes) {
+        this.numberOfBankNotes = numberOfBankNotes;
+    }
+
+    public String getTypeOfBankNote() {
+        return typeOfBankNote;
+    }
+
+    public void setTypeOfBankNote(String typeOfBankNote) {
+        this.typeOfBankNote = typeOfBankNote;
+    }
+
+    @Override
+    public String toString() {
+        return "Cash{" +
+                "paymentId='" + paymentId + '\'' +
+                ", numberOfBankNotes=" + numberOfBankNotes +
+                ", typeOfBankNote='" + typeOfBankNote + '\'' +
+                '}';
+    }
 }
--- src/model/Customer.java
+++ src/model/Customer.java
@@ -1,5 +1,104 @@
 package model;
 
 public class Customer {
+    private String phoneNumber;
+    private String password;
+    private String email;
+    private String firstName;
+    private String lastName;
+    private String city;
+    private String state;
+    private String zipCode;
 
+    public Customer() {
+    }
+
+    public Customer(String phoneNumber, String password, String email, String firstName, String lastName, String city, String state, String zipCode) {
+        this.phoneNumber = phoneNumber;
+        this.password = password;
+        this.email = email;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.city = city;
+        this.state = state;
+        this.zipCode = zipCode;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipCode() {
+        return zipCode;
+    }
+
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Customer{" +
+                "phoneNumber='" + phoneNumber + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", city='" + city + '\'' +
+                ", state='" + state + '\'' +
+                ", zipCode='" + zipCode + '\'' +
+                '}';
+    }
 }
