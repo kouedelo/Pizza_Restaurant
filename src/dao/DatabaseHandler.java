66d6472426c0d49472d8e74ba523b1b63ac96426
kouedelo <kouedelo37@gmail.com>
2021-11-29 15:50:48 -0500

    Creation of beverage page.

--- src/dao/DatabaseHandler.java
+++ src/dao/DatabaseHandler.java
@@ -25,7 +25,7 @@ public class DatabaseHandler {
     // Mysql Database URL
     private static final String DB_URL = "jdbc:mysql://localhost:3306/pizza_restaurant?zeroDateTimeBehavior=CONVERT_TO_NULL&serverTimezone=UTC&useSSL=false";
     private static final String username = "root";
-    private static final String password = "!Fo06052002!";
+    private static final String password = "sep@2398";
     private static Connection conn = null;
     private static Statement stmt = null;
 
--- src/view/PizzaRestaurantMain.java
+++ src/view/PizzaRestaurantMain.java
@@ -16,7 +16,7 @@ public class PizzaRestaurantMain extends Application {
 
     @Override
     public void start(Stage primaryStage) throws IOException {
-        Parent root = FXMLLoader.load(getClass().getResource("pizzaPage.fxml"));
+        Parent root = FXMLLoader.load(getClass().getResource("orderPage.fxml"));
 
         Scene scene = new Scene(root);
 
