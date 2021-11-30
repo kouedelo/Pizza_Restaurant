e8c88f8c62c88498f38781f1ed206b8979c3dadc
jmstogsdill <jmstogsdill@comcast.net>
2021-11-30 09:15:45 -0500

    Added controllers and fxml files for the Checkout and Account Settings screens.

---
+++ src/view/CheckoutPageController.java
@@ -0,0 +1,45 @@
+package view;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+
+public class CheckoutPageController {
+
+    @FXML
+    private TextField addressTxtField;
+
+    @FXML
+    private TextField cardNumTxtField;
+
+    @FXML
+    private TextField cityTxtField;
+
+    @FXML
+    private TextField expirationTextField;
+
+    @FXML
+    private ChoiceBox<String> orderTypeChoiceBox = new ChoiceBox<>();
+    ObservableList<String> availableOrderTypeChoices = FXCollections.observableArrayList("Delivery", "Pick-up");
+    orderTypeChoiceBox.setItems(availableOrderTypeChoices);
+
+    @FXML
+    private ChoiceBox<String> paymentTypeChoiceBox = new ChoiceBox<>();
+    ObservableList<String> availablePaymentTypeChoices = FXCollections.observableArrayList("Cash", "Credit or Debit Card", "Check");
+    paymentTypeChoiceBox.setItems(availablePaymentTypeChoices);
+
+    @FXML
+    private TextField securityTxtField;
+
+    @FXML
+    private TextField zipTxtField;
+
+    @FXML
+    void handleCancelButtonAction(ActionEvent event) {
+
+    }
+
+}
